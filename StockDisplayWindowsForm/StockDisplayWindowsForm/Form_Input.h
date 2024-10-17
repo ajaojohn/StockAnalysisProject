bcef63bca5f96de5c4fd0c44f2a577599762237b
ajaojohn <johnajao14@gmail.com>
2024-10-17 10:21:49 -0400

    Rename openFileDialog, set filters, and set default directory

--- StockDisplayWindowsForm/StockDisplayWindowsForm/Form_Input.h
+++ StockDisplayWindowsForm/StockDisplayWindowsForm/Form_Input.h
@@ -36,7 +36,8 @@ namespace CppCLRWinFormsProject {
 		}
 	private: System::Windows::Forms::DataGridView^ dataGridView_stockData;
 	private: System::Windows::Forms::Button^ button_selectFile;
-	private: System::Windows::Forms::OpenFileDialog^ openFileDialog_selectStock;
+	private: System::Windows::Forms::OpenFileDialog^ openFileDialog_load;
+
 	protected:
 
 
@@ -55,41 +56,46 @@ namespace CppCLRWinFormsProject {
 		{
 			this->dataGridView_stockData = (gcnew System::Windows::Forms::DataGridView());
 			this->button_selectFile = (gcnew System::Windows::Forms::Button());
-			this->openFileDialog_selectStock = (gcnew System::Windows::Forms::OpenFileDialog());
+			this->openFileDialog_load = (gcnew System::Windows::Forms::OpenFileDialog());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView_stockData))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// dataGridView_stockData
 			// 
 			this->dataGridView_stockData->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
-			this->dataGridView_stockData->Location = System::Drawing::Point(12, 38);
+			this->dataGridView_stockData->Location = System::Drawing::Point(9, 31);
+			this->dataGridView_stockData->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->dataGridView_stockData->Name = L"dataGridView_stockData";
 			this->dataGridView_stockData->RowHeadersWidth = 51;
 			this->dataGridView_stockData->RowTemplate->Height = 24;
-			this->dataGridView_stockData->Size = System::Drawing::Size(1019, 496);
+			this->dataGridView_stockData->Size = System::Drawing::Size(764, 403);
 			this->dataGridView_stockData->TabIndex = 0;
 			// 
 			// button_selectFile
 			// 
-			this->button_selectFile->Location = System::Drawing::Point(423, 625);
+			this->button_selectFile->Location = System::Drawing::Point(317, 508);
+			this->button_selectFile->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->button_selectFile->Name = L"button_selectFile";
-			this->button_selectFile->Size = System::Drawing::Size(142, 44);
+			this->button_selectFile->Size = System::Drawing::Size(106, 36);
 			this->button_selectFile->TabIndex = 1;
 			this->button_selectFile->Text = L"Select File";
 			this->button_selectFile->UseVisualStyleBackColor = true;
 			this->button_selectFile->Click += gcnew System::EventHandler(this, &Form_Input::button_selectFile_Click);
 			// 
-			// openFileDialog_selectStock
+			// openFileDialog_load
 			// 
-			this->openFileDialog_selectStock->FileName = L"openFileDialog1";
-			// 
+			this->openFileDialog_load->FileName = L"openFileDialog1";
+			this->openFileDialog_load->Filter = L"CSV Files (*.csv)|*.csv|Monthly|*_Month.csv|Weekly|*_Week.csv|Daily|*_Day.csv";
+			System::String^ CombinedPath = System::IO::Path::Combine(System::IO::Directory::GetCurrentDirectory(), "..\\StockData"); // Create string for default path
+			this->openFileDialog_load->InitialDirectory = System::IO::Path::GetFullPath(CombinedPath); // Set default path
 			// Form_Input
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(1043, 724);
+			this->ClientSize = System::Drawing::Size(782, 588);
 			this->Controls->Add(this->button_selectFile);
 			this->Controls->Add(this->dataGridView_stockData);
+			this->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->Name = L"Form_Input";
 			this->Text = L"Form_Input";
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView_stockData))->EndInit();
@@ -100,7 +106,7 @@ namespace CppCLRWinFormsProject {
 
 	private: System::Void button_selectFile_Click(System::Object^ sender, System::EventArgs^ e) {
 		// On button click, trigger the open file dialog
-		openFileDialog_selectStock->ShowDialog();
+		openFileDialog_load->ShowDialog();
 	}
 	};
 }
