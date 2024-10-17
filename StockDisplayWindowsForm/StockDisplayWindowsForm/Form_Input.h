dc414fcd37454f52237a90fb266cd7acc13c1742
ajaojohn <johnajao14@gmail.com>
2024-10-17 10:51:44 -0400

    Rename button_selectFile to button_load

--- StockDisplayWindowsForm/StockDisplayWindowsForm/Form_Input.h
+++ StockDisplayWindowsForm/StockDisplayWindowsForm/Form_Input.h
@@ -37,7 +37,8 @@ namespace CppCLRWinFormsProject {
 			}
 		}
 	private: System::Windows::Forms::DataGridView^ dataGridView_stockData;
-	private: System::Windows::Forms::Button^ button_selectFile;
+	private: System::Windows::Forms::Button^ button_load;
+
 	private: System::Windows::Forms::OpenFileDialog^ openFileDialog_load;
 
 	protected:
@@ -57,7 +58,7 @@ namespace CppCLRWinFormsProject {
 		void InitializeComponent(void)
 		{
 			this->dataGridView_stockData = (gcnew System::Windows::Forms::DataGridView());
-			this->button_selectFile = (gcnew System::Windows::Forms::Button());
+			this->button_load = (gcnew System::Windows::Forms::Button());
 			this->openFileDialog_load = (gcnew System::Windows::Forms::OpenFileDialog());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView_stockData))->BeginInit();
 			this->SuspendLayout();
@@ -65,37 +66,38 @@ namespace CppCLRWinFormsProject {
 			// dataGridView_stockData
 			// 
 			this->dataGridView_stockData->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
-			this->dataGridView_stockData->Location = System::Drawing::Point(9, 31);
-			this->dataGridView_stockData->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
+			this->dataGridView_stockData->Location = System::Drawing::Point(16, 57);
+			this->dataGridView_stockData->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->dataGridView_stockData->Name = L"dataGridView_stockData";
 			this->dataGridView_stockData->RowHeadersWidth = 51;
 			this->dataGridView_stockData->RowTemplate->Height = 24;
-			this->dataGridView_stockData->Size = System::Drawing::Size(764, 403);
+			this->dataGridView_stockData->Size = System::Drawing::Size(1401, 744);
 			this->dataGridView_stockData->TabIndex = 0;
 			// 
-			// button_selectFile
+			// button_load
 			// 
-			this->button_selectFile->Location = System::Drawing::Point(317, 508);
-			this->button_selectFile->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
-			this->button_selectFile->Name = L"button_selectFile";
-			this->button_selectFile->Size = System::Drawing::Size(106, 36);
-			this->button_selectFile->TabIndex = 1;
-			this->button_selectFile->Text = L"Select File";
-			this->button_selectFile->UseVisualStyleBackColor = true;
-			this->button_selectFile->Click += gcnew System::EventHandler(this, &Form_Input::button_selectFile_Click);
+			this->button_load->Location = System::Drawing::Point(581, 938);
+			this->button_load->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->button_load->Name = L"button_load";
+			this->button_load->Size = System::Drawing::Size(194, 66);
+			this->button_load->TabIndex = 1;
+			this->button_load->Text = L"Select File";
+			this->button_load->UseVisualStyleBackColor = true;
+			this->button_load->Click += gcnew System::EventHandler(this, &Form_Input::button_selectFile_Click);
 			// 
 			// openFileDialog_load
 			// 
 			this->openFileDialog_load->FileName = L"openFileDialog1";
 			this->openFileDialog_load->Filter = L"CSV Files (*.csv)|*.csv|Monthly|*_Month.csv|Weekly|*_Week.csv|Daily|*_Day.csv";
+			// 
 			// Form_Input
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleDimensions = System::Drawing::SizeF(11, 24);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(782, 588);
-			this->Controls->Add(this->button_selectFile);
+			this->ClientSize = System::Drawing::Size(1434, 1086);
+			this->Controls->Add(this->button_load);
 			this->Controls->Add(this->dataGridView_stockData);
-			this->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
+			this->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->Name = L"Form_Input";
 			this->Text = L"Form_Input";
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView_stockData))->EndInit();
