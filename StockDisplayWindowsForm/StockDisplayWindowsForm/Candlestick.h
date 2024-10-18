adc1d824fa98e92871fa9fe1dccfa7ecaa1ecd3a
ajaojohn <johnajao14@gmail.com>
2024-10-18 15:30:59 -0400

    New candlestick constructor and function/format

--- StockDisplayWindowsForm/StockDisplayWindowsForm/Candlestick.h
+++ StockDisplayWindowsForm/StockDisplayWindowsForm/Candlestick.h
@@ -1,58 +1,85 @@
+namespace CppCLRWinFormsProject {
 #pragma once
-ref class Candlestick
-{
-public:
-    // Properties for a candlestick
-    System::DateTime^ date;  // Time of the candlestick
-    double open;          // Opening price
-    double high;          // Highest price
-    double low;           // Lowest price
-    double close;         // Closing price
-    double volume;        // Volume of stock traded
-
-    /**
-    * Default constructor
-    */
-    Candlestick()
+    ref class Candlestick
     {
-		this->date = gcnew System::DateTime();
-		this->open = 0;
-		this->high = 0;
-		this->low = 0;
-		this->close = 0;
-		this->volume = 0;
-    }
+    public:
+        // Properties for a candlestick
+        System::DateTime^ date;  // Date of the candlestick
+        System::Decimal open;    // Opening price
+        System::Decimal high;    // Highest price
+        System::Decimal low;     // Lowest price
+        System::Decimal close;   // Closing price
+        System::Int64 volume;    // Volume of stock traded
 
-    /**
-    * Candlestick default constructor
-    * @param date Time of the candlestick
-    * @param open Opening price
-    * @param high Highest price
-    * @param low Lowest price
-    * @param close Closing price
-    * @param volume Volume of stock traded
-    */
-    Candlestick(System::DateTime^ date, double open, double high, double low, double close, double volume)
-    {
-        this->date = date;
-        this->open = open;
-        this->high = high;
-        this->low = low;
-        this->close = close;
-        this->volume = volume;
-    }
+        /**
+        * Default constructor
+        */
+        Candlestick()
+        {
+            setCandlestickProperties(gcnew System::DateTime(), System::Decimal(0), System::Decimal(0), System::Decimal(0), System::Decimal(0), 0);
+        }
 
-    /**
-    * Copy constructor
-	* @param candlestick The candlestick to copy
-    */
-    Candlestick(Candlestick^ candlestick) {
-		this->date = candlestick->date;
-		this->open = candlestick->open;
-		this->high = candlestick->high;
-		this->low = candlestick->low;
-		this->close = candlestick->close;
-		this->volume = candlestick->volume;
-    }
-};
+        /**
+        * Candlestick constructor
+        * @param Line from a csv file formatted "date,open,high,low,close,adj close,volume"
+        */
+        Candlestick(System::String^ row)
+        {
+            // Split the row into an array
+            array<System::String^>^ splitRow = row->Split(',');
 
+			// Set the properties
+            setCandlestickProperties(
+                System::DateTime::Parse(splitRow[0]),
+                System::Convert::ToDecimal(splitRow[1]),
+                System::Convert::ToDecimal(splitRow[2]),
+                System::Convert::ToDecimal(splitRow[3]),
+                System::Convert::ToDecimal(splitRow[4]),
+                System::Convert::ToInt64(splitRow[6])
+            );
+        }
+
+        /**
+        * Candlestick constructor
+        * @param date Time of the candlestick
+        * @param open Opening price
+        * @param high Highest price
+        * @param low Lowest price
+        * @param close Closing price
+        * @param volume Volume of stock traded
+        */
+        Candlestick(System::DateTime^ date, System::Decimal open, System::Decimal high, System::Decimal low, System::Decimal close, System::Int64 volume)
+        {
+            setCandlestickProperties(date, open, high, low, close, volume);
+        }
+
+        /**
+        * Copy constructor
+        * @param candlestick The candlestick to copy
+        */
+        Candlestick(Candlestick^ candlestick) {
+            setCandlestickProperties(candlestick->date, candlestick->open, candlestick->high, candlestick->low, candlestick->close, candlestick->volume);
+        }
+
+    private:
+        /**
+        * Set the candlestick properties
+        * @param date Date of the candlestick
+        * @param open Opening price
+        * @param high Highest price
+        * @param low Lowest price
+        * @param close Closing price
+        * @param volume Volume of stock traded
+        * @returns void
+        */
+        System::Void setCandlestickProperties(System::DateTime^ date, System::Decimal open, System::Decimal high, System::Decimal low, System::Decimal close, System::Int64 volume) {
+            // Set properties from parameters
+		    this->date = date;
+		    this->open = open;
+		    this->high = high;
+		    this->low = low;
+		    this->close = close;
+		    this->volume = volume;
+        }
+    };
+}
